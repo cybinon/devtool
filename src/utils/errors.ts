49056326618918f36b1c0531fb5b703c76aee55c
cybinon <nikorunikk@gmail.com>
2022-02-13 13:03:56 +0800

    done

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -1,7 +1,12 @@
 import { writeFileSync } from 'fs';
-export const GetErrorMessage = () => {
+import axios from 'axios';
+export const GetErrorMessage = async () => {
   try {
-    const file = writeFileSync('./errors.json', '');
+    const data = await axios
+      .get('https://raw.githubusercontent.com/cybinon/devtool/main/json/error.json')
+      .then((res) => res.data);
+    writeFileSync('./errors.json', data);
+    console.log('Generated Errors');
   } catch (e) {
     console.log(e);
   }
